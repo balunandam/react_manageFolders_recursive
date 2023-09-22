55de7d04b3ce493257e12dfa5dc9ba6ea199bca9
baladithya.nandam <baladithya.nandam@kanerika.com>
2023-09-22 13:01:52 +0530

    file and folder structure changes

--- src/components/File.js
+++ src/components/File.js
@@ -1,28 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   IconPencil,
   IconCheck,
   IconX,
   IconFolderPlus,
-  IconFilePlus
+  IconFilePlus,
 } from "@tabler/icons-react";
 export default function File({ data }) {
   const [flag, setFlag] = useState(false);
   let [editEnable, editDisable] = useState(true);
   const [addNew, setAddNew] = useState(false);
-  const [addNewName, setAddNewName] = useState('');
+  const [addNewName, setAddNewName] = useState("");
   let [newName, setNewName] = useState(data.name);
   const [addType, setAddType] = useState();
+  const [incomingItems, setIncomingItems] = useState(data.items);
   const handleChange = (event) => {
     setNewName(event.target.value);
   };
   const addNewFolder = (type) => {
     setAddNew(!addNew);
     setAddType(type);
-  }
+  };
   const handleSetNewName = (event) => {
     setAddNewName(event.target.value);
-  }
+  };
   if (data.type) {
     return (
       <div>
@@ -51,17 +52,23 @@ export default function File({ data }) {
                   postion: "absolute",
                   marginLeft: "2px",
                 }}
-                onClick={() => {addNewFolder('folder'); setFlag(true)}}
+                onClick={() => {
+                  addNewFolder("folder");
+                  setFlag(true);
+                }}
               />
-               <IconFilePlus
+              <IconFilePlus
                 size={12}
                 style={{
                   postion: "absolute",
                   marginLeft: "2px",
                 }}
-                onClick={() => addNewFolder('file')}
+                onClick={() => {
+                  addNewFolder("file");
+                  setFlag(true);
+                }}
               />
-              </div>
+            </div>
           ) : (
             <div>
               <input
@@ -89,15 +96,21 @@ export default function File({ data }) {
                   marginLeft: "2px",
                 }}
                 onClick={() => {
-                    editDisable(true);
-                  }}
+                  editDisable(true);
+                }}
               />
             </div>
           )}
         </div>
         <div style={{ display: flag ? "block" : "none", paddingLeft: "15px" }}>
-            <input type="text" value={addNewName} onChange={handleSetNewName}/>
-            <IconCheck
+          {addType &&
+        <div>
+              <input
+                type="text"
+                value={addNewName}
+                onChange={handleSetNewName}
+              />
+              <IconCheck
                 size={12}
                 style={{
                   marginTop: "7px",
@@ -105,30 +118,41 @@ export default function File({ data }) {
                   marginLeft: "2px",
                 }}
                 onClick={() => {
-                  data.items.push({name: addNewName, type: addType});
-                  console.log(data.items);
+                    debugger
+                  const newItem = {
+                    name: addNewName,
+                    type: addType === "folder" ? true : false,
+                  };
+                  if (addType === "folder") {
+                    newItem["items"] = [];
+                  }
+                  incomingItems.push(newItem);
+                  setIncomingItems(incomingItems);
+                  setAddType();
                 }}
               />
-                <IconX
+              <IconX
                 size={12}
                 style={{
                   marginTop: "7px",
                   postion: "absolute",
                   marginLeft: "2px",
                 }}
-                // onClick={() => {
-
-                // }}
+                onClick={() => {
+                  setAddType();
+                }}
               />
-          {data.items.map((res) => {
+            </div>
+        }
+          {incomingItems.map((res) => {
             return <File data={res} />;
           })}
         </div>
       </div>
     );
   } else {
-    <div>
-      <span>{newName}</span>
+    return <div>
+      <span>{data.name}</span>
     </div>;
   }
 }
